8fb93bfc422fbc26b6436b1db057d4515e2eb5d3
George K. Thiruvathukal <gthiruvathukal@luc.edu>
2023-01-06 21:49:03 -0600

    add some comments about the Gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 
 plugins {
     java
+
+    // https://docs.gradle.org/current/userguide/application_plugin.html
     application
+
+    // https://docs.gradle.org/current/userguide/distribution_plugin.html
+    distribution
 }
 
 group = "sh.gkt"
@@ -12,11 +17,19 @@ repositories {
 }
 
 dependencies {
+    // Unit Testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    // We use Awesome Java to choose helpful libraries: https://github.com/akullpp/awesome-java
+    // Command-Line Interface
     implementation("info.picocli:picocli:4.7.0")
+
+    // Lightweight logging
     implementation("org.tinylog:tinylog-api:2.5.0")
     implementation("org.tinylog:tinylog-impl:2.5.0")
+
+    // Console I/O (not using this yet)
     implementation("org.jline:jline:3.21.0")
 }
 
